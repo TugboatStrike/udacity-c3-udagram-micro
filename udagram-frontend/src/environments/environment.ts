cbe89d8ef8fe13f7dc915bde05251fa7639bc3cc
Byron Meals <bmeals87@gmail.com>
2022-07-23 16:31:11 -0600

    Updated frontend api host reverseproxy end point address from localhost

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  //apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aaa7052267ecb4e4aaf52b70006da056-592850170.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
